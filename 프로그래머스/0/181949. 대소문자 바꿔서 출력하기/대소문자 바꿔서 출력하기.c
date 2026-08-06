cc209215bbd18509a6d07a16f638ea164fff2278
이가영 <103436798+gayoung228@users.noreply.github.com>
2026-08-06 14:18:24 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 1.70 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define LEN_INPUT 10
+
+int main(void) {
+    char s1[LEN_INPUT];
+    scanf("%s", s1);
+    int len = strlen(s1);
+    
+    for(int i=0; i<len; i++){
+        if(s1[i]<97){ //대문자라면
+            s1[i]+=32;
+        }
+        else if(s1[i]>=97){
+            s1[i]-=32;
+        }
+    }
+    printf("%s", s1);
+
+    return 0;
+}
